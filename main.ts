de3c8e101ae9d5f6a4d2ae21bf6f0f3eb6a3f962
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-02-24 13:00:36 -0500

    Created Program

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jet Lu
+ * Created on: Feb 2026
+ * This program will display the temperature in kelvin.
 */
+let temperature: number
+let temperatureKelvin: number
 
-basic.showString('Hello, World!')
+//reset
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Temperature (in kelvin) will display when A is pressed
+input.onButtonPressed(Button.A, function () {
+    temperature = input.temperature()
+    temperatureKelvin = Math.round(temperature + 273.15)
+    basic.showString("The temperature is" + temperatureKelvin + " K")
+    pause(1000)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
